d3a8a608fa0be5fa45d83ef01c5841f6b77dbb6d
Bello Shehu Ango <angobello0@gmail.com>
2024-08-06 14:49:33 +0100

    improv: remove app sleep

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-sleep 3 # Sleep 3 seconds for railway private url to init
-
 alembic upgrade head
 
-uvicorn app.main:app --host "0.0.0.0" --port $PORT
+fastapi run --port $PORT
